7acffe46376e2e2367dc1ed1d785eabd09894685
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2019-12-21 22:21:17 -0500

    Update RadioStationCollectionInterface.java

--- radioTraversal/RadioStationCollectionInterface.java
+++ radioTraversal/RadioStationCollectionInterface.java
@@ -3,9 +3,7 @@ package radioTraversal;
 public interface RadioStationCollectionInterface {
 	
 	public void addRadioStation(RadioStationSong rs);
-	
 	public void removeRadioStation(RadioStationSong rs);
-	
 	public RadioStationIteratorInterface iterator(RadioStationTypeEnum type);
 }
 
