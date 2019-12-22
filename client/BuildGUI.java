cf723ca4b65a9e0a26d1aff1aa7f20584070e563
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2019-12-21 22:15:17 -0500

    Update BuildGUI.java

--- client/BuildGUI.java
+++ client/BuildGUI.java
@@ -14,8 +14,8 @@ import javax.swing.WindowConstants;
 public class BuildGUI extends JFrame {
 	
 
-	private static final long serialVersionUID = -4331421966781835517L;
-	protected JButton chillBtn = new JButton("CHILL");
+    private static final long serialVersionUID = -4331421966781835517L;
+    protected JButton chillBtn = new JButton("CHILL");
     protected JButton popBtn = new JButton("POP");
     protected JButton edmBtn = new JButton("EDM");
     protected JButton britneyBtn = new JButton("BRITNEY");
@@ -25,12 +25,11 @@ public class BuildGUI extends JFrame {
     protected JPanel panel4 = new JPanel();
     protected JTextArea output = new JTextArea();
     protected JButton stopBtn = new JButton("Stop");
-    
     private static BuildGUI singleGUI = null;
 
-	// constructor
+	// Constructor
 	private BuildGUI(){ 
-		this.setTitle("OOD Final Project");
+		this.setTitle("Media Player");
 		this.setSize(500, 300);
 		this.getContentPane().setLayout(
                 new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
@@ -38,18 +37,18 @@ public class BuildGUI extends JFrame {
 		
 		JPanel panel1 = new JPanel();
 		panel1.setPreferredSize(new Dimension(900,10));
-        this.add(panel1);
+        	this.add(panel1);
         
-        JLabel label1 = new JLabel("Select a Radio Station");
-        panel1.add(label1);
+        	JLabel label1 = new JLabel("Select a Radio Station");
+       	 	panel1.add(label1);
 		
-        JPanel panel2 = new JPanel();
-        panel2.setPreferredSize(new Dimension(500,10));
-        this.add(panel2);
+        	JPanel panel2 = new JPanel();
+        	panel2.setPreferredSize(new Dimension(500,10));
+        	this.add(panel2);
         
-        JPanel panel3 = new JPanel();
-        panel3.setPreferredSize(new Dimension(500,10));
-        this.add(panel3);
+        	JPanel panel3 = new JPanel();
+        	panel3.setPreferredSize(new Dimension(500,10));
+        	this.add(panel3);
 
 		panel4.setPreferredSize(new Dimension(500,10));
 		this.add(panel4);
@@ -62,24 +61,20 @@ public class BuildGUI extends JFrame {
 		this.add(panel5);
 		output.setLineWrap(true);
 		output.setEditable(false);
-//		output.setPreferredSize(preferredSize);
 		JScrollPane scroll = new JScrollPane(output);
 		scroll.setPreferredSize(new Dimension (310,100));
 		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		
 		panel5.add(scroll);
+        	panel2.add(chillBtn);
+        	panel2.add(popBtn);
+        	panel2.add(edmBtn);
+        	panel3.add(britneyBtn);
+        	panel3.add(rhcpBtn);
+        	panel3.add(allBtn);
         
-        panel2.add(chillBtn);
-        panel2.add(popBtn);
-        panel2.add(edmBtn);
-        
-        panel3.add(britneyBtn);
-        panel3.add(rhcpBtn);
-        panel3.add(allBtn);
-        
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        	this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setVisible(true);
-		
 		setSingleGUI(this);	
 	}
 
